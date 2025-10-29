a29a4c33a37aa9a7fdfd5cdca8e2a9142f8b1964
aliakbarsobhanpoor <aliakbarsobhanpoor@gmail.com>
2025-10-29 16:11:56 +0330

    change the project structure to use new versions of django.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,30 +1,45 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import ChatChannels, ChatMessage
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
-from asgiref.sync import async_to_sync
+from django.http import Http404
+from asgiref.sync import sync_to_async
 import json
 
 
-class ChatChannelConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatChannelConsumer(AsyncWebsocketConsumer):
+    @staticmethod
+    async def aget_object_or_404(klass, *args, **kwargs):    
+        if hasattr(klass, 'aget'):
+            queryset = klass
+            model_name = queryset.model._meta.object_name
+        else:
+            queryset = klass.objects
+            model_name = klass._meta.object_name
+
+        try:
+            return await queryset.aget(*args, **kwargs)
+        except queryset.model.DoesNotExist:
+            raise Http404(f"No {model_name} matches the given query.")
+        
+    async def connect(self):
         self.user = self.scope["user"]
         self.chat_channel_name = self.scope["url_route"]["kwargs"]["channel_name"]
-        self.channel = get_object_or_404(ChatChannels, channel_name=self.chat_channel_name)
-        async_to_sync(self.channel_layer.group_add)(
+        self.channel = await self.aget_object_or_404(ChatChannels, channel_name=self.chat_channel_name)
+        await self.channel_layer.group_add(
             self.chat_channel_name, self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.chat_channel_name, self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json.get("message", "")
-        saved_message = ChatMessage.objects.create(
+        saved_message = await ChatMessage.objects.acreate(
             message=message,
             author=self.user,
             channel=self.channel
@@ -34,14 +49,19 @@ class ChatChannelConsumer(WebsocketConsumer):
             "type": "message_handler",
             "message_id": saved_message.id
         }
-        
-        async_to_sync(self.channel_layer.group_send)(self.chat_channel_name, event)
-        
-    def message_handler(self, event):
-        message_id = event.get("message_id")
-        message = get_object_or_404(ChatMessage, id=message_id)
-        html = render_to_string("chat/partial/message_partial.html", {
+
+        await self.channel_layer.group_send(self.chat_channel_name, event)
+    
+    @staticmethod
+    @sync_to_async
+    def render_message_html(message, user):
+        return render_to_string("chat/partial/message_partial.html", {
             "message": message, 
-            "user": message.author
-            })
-        self.send(text_data=html)
+            "user": user
+        })
+    
+    async def message_handler(self, event):
+        message_id = event.get("message_id")
+        message = await self.aget_object_or_404(ChatMessage, id=message_id)
+        html = await self.render_message_html(message, self.user) 
+        await self.send(text_data=html)
